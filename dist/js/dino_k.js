9d3c42109e92dadb4b5ff5ab4eaf18e1aa057cf4
yuenchang <yuenangel1124@gmail.com>
2020-05-17 17:42:28 +0800

    send letter added

--- dist/js/dino_k.js
+++ dist/js/dino_k.js
@@ -2,11 +2,29 @@ var wip = "wss://" + window.location.host;
 var socket = io(wip);
 
 $(document).ready(function () {
-    $('#letter').html("謝謝" + getCookie('nickname') + "\n因為...\n這讓我覺得...");
+    $('#letter').html("\n  謝謝" + getCookie('nickname') + "\n\n  因為...\n\n  這讓我覺得...");
 });
 
-function letter(){
-    $('.write_letter').fadeIn(400);
+function go_to_letter(){
+    $('.write_letter').fadeIn(800);
+    $('.info').fadeOut(400);
+    $('#cover').css('display','none'); //關閉遮罩層
+
+    //屁頭上升
+    $('#ass_dinasour').animate({top:"-=23vh" },800);
+}
+
+function letter_info(){
+    $('#cover').css('display','block'); //顯示遮罩層
+    $('.info').fadeIn(400);
+    $('.info').css('z-index', 5);
+    $('#right_leaf').animate({left:"+=200vw" },1500);
+    $('#training_logo').animate({left:"+=200vw" },1500);
+    $('#training_text').animate({left:"+=200vw" },1500);
+    $('#left_leaf').animate({left:"-=200vw" },1500);
+    $('#mission_logo').animate({left:"-=200vw" },1500);
+    $('#mission_text').animate({left:"-=200vw" },1500);
+    //$('.write_letter').fadeIn(400);
 }
 
 function write_letter(){
@@ -14,11 +32,41 @@ function write_letter(){
     window.location.href='letter_k.html';
 }
 
+function letter_back(){
+    $('.write_letter').fadeOut(800);
+    $('#right_leaf').animate({left:"-=200vw" },1500);
+    $('#training_logo').animate({left:"-=200vw" },1500);
+    $('#training_text').animate({left:"-=200vw" },1500);
+    $('#left_leaf').animate({left:"+=200vw" },1500);
+    $('#mission_logo').animate({left:"+=200vw" },1500);
+    $('#mission_text').animate({left:"+=200vw" },1500);
+    $('#ass_dinasour').animate({top:"+=23vh" },800);
+}
+
 function send_letter(){
-    var letter = $('#letter').val();
-    alert(letter);
-    var id = getCookie('ID');
-    socket.emit('send_letter', {ID: id, Letter: letter});
+    //寄信
+    //var letter = $('#letter').val();
+    //var id = getCookie('ID');
+    //socket.emit('send_letter', {ID: id, Letter: letter});
+
+    // 動畫
+    //$('#letter_bg').animate({height:"-=20vh"}, 1200);
+    $('.write_letter').css('display','none');
+    $('#letter_bg_rec').css('display','block');
+    $('#letter_bg_rec').animate({top:"+=22vh", left:"+=15vw", height:"-=43vh", width:"-=30vw"}, 800);
+    $('#ass_dinasour').animate({top:"+=23vh" },800);
+    $('#sent').fadeIn(1200);
+    $('#right_leaf').animate({left:"-=200vw" },1500);
+    $('#training_logo').animate({left:"-=200vw" },1500);
+    $('#training_text').animate({left:"-=200vw" },1500);
+    $('#left_leaf').animate({left:"+=200vw" },1500);
+    $('#mission_logo').animate({left:"+=200vw" },1500);
+    $('#mission_text').animate({left:"+=200vw" },1500);
+    $('#sent').fadeOut(2000);
+    $('#letter_bg_rec').fadeOut(2000);
+
+    //圖片大小復原
+    $('#letter_bg_rec').animate({top:"-=22vh", left:"-=15vw", height:"+=43vh", width:"+=30vw"}, 10);
 }
 
 
